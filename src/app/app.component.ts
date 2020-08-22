0f5417abf4d8f7537b7a99d74bb104cd36e8d95a
geekysrm <soumyacool2012@gmail.com>
2020-08-22 21:18:24 +0530

    add posts array of objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      content: 'Awesome tree seen during hike.',
+      imageUrl: 'assets/tree.jpeg',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'mountain_lover',
+      content: 'Pic of a snowy mountain!',
+      imageUrl: 'assets/mountain.jpeg',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking99',
+      content: 'Had a lot of fun during biking.',
+      imageUrl: 'assets/biking.jpeg',
+    },
+  ];
 }
