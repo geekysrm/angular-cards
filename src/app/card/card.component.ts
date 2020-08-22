2101b680fd2fd825a8782a1ce6ac4665117a6625
geekysrm <soumyacool2012@gmail.com>
2020-08-22 21:47:54 +0530

    pass data from app to card component and use it in card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  // receiving below 4 properties from app component template html
+  @Input() title = '';
+  @Input() username = '';
+  @Input() content = '';
+  @Input() imageUrl = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
